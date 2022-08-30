0c19dccc0ef56770a849d310da14311b45e81982
Jason Fakour <jfakour@outlook.com>
2022-08-30 18:43:59 +0200

    Initial Commit. MVP Features implemented

---
+++ Unzipper/Main.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright Jason Fakour
+ * Aug 30, 2022
+ * All rights reserved
+ */
+
+using System.IO.Compression;
+
+namespace Unzipper
+{
+    class UnZipperDriver
+    {
+        static void Main(string[] args)
+        {
+            List<string> arguments = new List<string>(args);
+
+
+            if (arguments.Count == 0)
+            {
+                displayWelcomeMsg();
+                Console.Write("Full path to directory containing zip files: ");
+                string dp = Console.ReadLine() ?? Directory.GetCurrentDirectory();
+                arguments.Add(dp);
+
+            }
+
+            Dictionary<string, string> extractedZipFiles = new Dictionary<string, string>();
+
+            foreach (string dirPath in arguments)
+            {
+                extractedZipFiles = extractedZipFiles
+                    .Concat(Unzipper.UnzipFiles(dirPath))
+                    .ToDictionary(x=> x.Key, x=> x.Value);
+            }
+
+            foreach (string fp in extractedZipFiles.Keys)
+            {
+                Console.WriteLine("* Extracting \"" + fp + "\" To \"" + extractedZipFiles[fp] + "\"") ;
+            }
+
+        }
+
+        static private void displayWelcomeMsg()
+        {
+            string msg = "**********************************************************\n" +
+                         "***!!!!!           PROCEED WITH CAUTION           !!!!!***\n" +
+                         "**********************************************************\n" +
+                         "* This application will recursively unzip all .zip files *\n" +
+                         "*  from a given directory                                *\n" +
+                         "*                                                        *\n" +
+                         "* DO NOT USE A ROOT-LIKE PATH!!!                         *\n" +
+                         "**********************************************************\n";
+            Console.Write(msg);
+        }
+    }
+}
+
---
+++ Unzipper/Unzipper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO.Compression;
+/*
+ * Copyright Jason Fakour
+ * Aug 30, 2022
+ * All rights reserved
+ */
+
+namespace Unzipper
+{
+    public static class Unzipper
+    {
+        /// <summary>
+        /// Recursively collects all the .zip files and extracts them.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>Dictionary<string: zip src, string: extracted dir></returns>
+        public static Dictionary<string, string> UnzipFiles(string path)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            List<string> allFiles = GetAllFilesFromPath(path);
+
+            Regex reg = new Regex(@"(?i)(.*\.zip)");
+
+            foreach (string fp in allFiles)
+            {
+                string fn = Path.GetFileName(fp);
+
+
+                if (reg.IsMatch(fn))
+                {
+                    string dirXtract = Path.GetDirectoryName(fp) ?? "";
+                    string fXtractName = Path.GetFileNameWithoutExtension(fp);
+                    string xtractPath = Path.Combine(dirXtract, fXtractName);
+
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(fp, xtractPath);
+                        result.Add(fp, xtractPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+            }
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Recursively collects ALL files in a directory
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>List<string> of all files in directory and subdirectories of path</string></returns>
+        public static List<string> GetAllFilesFromPath(string path)
+        {
+            List<string> result = new List<string>();
+
+            try
+            {
+                result.AddRange(Directory.GetFiles(path));
+
+                foreach (string str in Directory.GetDirectories(path))
+                {
+                    result.AddRange(GetAllFilesFromPath(str));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return result;
+        }
+
+    }
+}
